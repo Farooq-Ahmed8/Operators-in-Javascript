6b6174397ffaf4073c9366e9a23a80445a8d634c
Farooq Ahmed <03174748082ali@gmail.com>
2025-06-27 23:42:48 -0700

    work completed

---
+++ app.js
@@ -0,0 +1,79 @@
+//  Operators in Javascript
+
+// Arethmetic Operators
+
+//    Operator                      Description                        Example
+
+//    +                             Addition                           x + y   
+//    -                             Subtraction                        x - y
+//    *                             Multiplication                     x * y
+//    /                             Divisio                            x / y
+//    %                             Modulus (remainder)                x % y
+//    **                            Exponentiation                     x ** y
+//    ++                            Increment                          x++ or --x
+//    --                            Decrement                          x++ or --x
+
+
+
+// Assignment Operators
+
+// Ye operators variables k aunder value assign krne k leyee kam aaty hai
+
+//    Operator                      Description                        Example
+
+//    =                             Assignment                         x + 10
+//    +=                            Add and Assign                     x += 5
+//    -+                            Subtract and Assign                x -= 5
+//    *=                            Multiply and Assign                x *= 5
+//    /=                            Divide and Assign                  x /= 5
+//    %=                            Modulus and Assign                 x %= 5
+//    **=                           Exponentiatiion and Assign         x **= 5
+
+
+//Comparison Operators
+
+// Ye operators do2 value ka comparison kerty hai
+
+//    Operator                      Description                        Example
+
+//    ==                            Double equal to                    x == y
+//    ===                           Strict equal to                    x === y
+//    !=                            Not equal                          x != y
+//    !==                           Strict not equal                   x !== y
+//    >                             Greater than                       x > y
+//    <                             Less than                          x < y
+//    >=                            Greater than or equal              x >= y
+//    <=                            Less than or equal                 x <= y
+
+
+
+// Logical Operators
+
+// Ye operators serf Boolean value k leyee use hoty hain
+
+//    Operator                      Description                        Example
+
+//    &&                            Logical AND                        x && y
+//    !                             Logical AND                        x ! y
+
+
+
+// Ternary Operators
+
+// Ak shortcut tareqa if...else:
+
+// Condition ? exper1 : exper2;
+
+//  (e.g)
+
+// let age = 18;
+// let result = age >= 18 ? "Adult" : "Minor";
+// console.log(result);
+
+// let age = 20;
+// let result = age >= 18 ? "Adult" : "Minor";
+// console.log(result);
+
+
+
+ 
